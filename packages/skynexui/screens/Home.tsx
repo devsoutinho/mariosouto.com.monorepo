315fa3b6dc71b149229a22e62830442a5be5b4eb
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-03-19 18:43:03 -0300

    update

--- packages/skynexui/screens/Home.tsx
+++ packages/skynexui/screens/Home.tsx
@@ -1,20 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 import { Button } from '../index';
+
+const View = styled.View`
+  flex: 1;
+  background-color:#ff0;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.Text``;
 
 export function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Home Screen sample!</Text>
       <Button label="Custom Label works?" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
