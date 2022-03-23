3ae835375731d393ac6584c7b8f94eed9a56c4a2
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-03-22 21:47:14 -0300

    update

--- packages/skynexui/components/provider/provider.tsx
+++ packages/skynexui/components/provider/provider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSafeAreaInsets } from 'external-libs/react-native-safe-area-context/native';
 
-import { Platform, useWindowDimensions } from 'react-native';
+import { Platform, useWindowDimensions, Linking } from 'react-native';
 import { getCurrentBreakpoint } from '../../core/theme/breakpoints/breakpoints';
 import { defaultTheme, Theme } from '../../core/theme/defaultTheme';
 import { EnvCSS } from './EnvCSS';
@@ -26,6 +26,14 @@ export const useRouter = () => {
 
   return {
     push: (routeKey: string) => {
+      const isExternalURL = routeKey.startsWith('http');
+      if (isExternalURL) {
+        env.isWeb()
+          ? (globalThis as any).open(routeKey)
+          : Linking.openURL(routeKey);;
+        return;
+      }
+
       env.isWeb()
         ? router.push(routeKey)
         : router.navigate(routeKey);
@@ -36,17 +44,17 @@ export const useRouter = () => {
 export const useEnv = () => {
   return {
     themeCalc(operator: '+' | '-' = '+', ...args: any[]) {
-      if(this.isWeb()) {
+      if (this.isWeb()) {
         const values = args.map((arg) => typeof arg === 'number' ? `${arg}px` : arg);
-        if(operator === '+') return `calc(${values.join(' + ')})`;
-        if(operator === '-') return `calc(${values.join(' - ')})`;
+        if (operator === '+') return `calc(${values.join(' + ')})`;
+        if (operator === '-') return `calc(${values.join(' - ')})`;
       }
 
       const total = args.reduce((acc, curr) => {
         const currNormalized = typeof curr === 'string' ? Number(curr.replace('px', '')) : curr;
 
-        if(operator === '+') return acc + currNormalized;
-        if(operator === '-') return acc - currNormalized;
+        if (operator === '+') return acc + currNormalized;
+        if (operator === '-') return acc - currNormalized;
       }, 0);
 
       return `${total}px`;
