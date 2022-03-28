bdad49d37e68d694463b201e4c3ecb149c87b80c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-03-28 16:17:13 -0300

    update

--- apps/site/screens/HomeScreen/HomeScreen.tsx
+++ apps/site/screens/HomeScreen/HomeScreen.tsx
@@ -4,9 +4,22 @@ import { Scaffold } from 'skynexui/patterns/Scaffold/Scaffold';
 import { gql, useQuery } from '@apollo/client';
 import { addApolloState, initializeApollo } from 'external-libs/apollo-client';
 
+// function getYouTubeID(url) {
+//   const regex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+//   const match = url.match(regex);
+//   return match && match[7].length === 11 ? match[7] : false;
+// }
+
+// get youtube id
+const getYoutubeId = (url) => {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  const match = url.match(regExp);
+  return (match && match[7].length === 11) ? match[7] : false;
+};
+
 const GET_POSTS = gql`
-  query {
-    posts {
+  query($input: PostsInput) {
+    posts(input: $input) {
       title
       date
       url
@@ -14,11 +27,23 @@ const GET_POSTS = gql`
   }
 `;
 
+const variables = {
+  "input": {
+    "limit": 100,
+    "filter": {
+      "postType": {
+        "eq": "YOUTUBE_VIDEO"
+      }
+    }
+  }
+};
+
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
   await apolloClient.query({
     query: GET_POSTS,
+    variables,
   })
 
   return addApolloState(apolloClient, {
@@ -32,7 +57,9 @@ export function HomeScreen(props) {
   const theme = useTheme();
   const router = useRouter();
 
-  const { loading, error, data } = useQuery(GET_POSTS);
+  const { loading, error, data } = useQuery(GET_POSTS, {
+    variables,
+  });
 
   if (loading) return (
     <Box styleSheet={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.colors?.neutral?.x900, color: '#fff' }}>
@@ -86,74 +113,85 @@ export function HomeScreen(props) {
       <Box
         styleSheet={{
           maxWidth: theme.space?.xcontainer_sm,
-          alignItems: 'flex-start',
+          alignItems: 'center',
           marginTop: theme.space?.x6,
-          marginBottom: theme.space?.x6,
         }}
       >
-        <Text tag='h2' variant='body1' styleSheet={{ color: theme.colors?.neutral.x300 }}>
+        <Text
+          tag='h2'
+          variant='body1'
+          styleSheet={{
+            alignSelf: 'flex-start',
+            color: theme.colors?.neutral.x100,
+            marginBottom: theme.space?.x4,
+            // borderBottomStyle: 'solid',
+            borderBottomWidth: theme.space?.x1,
+            borderBottomColor: theme.colors?.primary.x600,
+            paddingBottom: theme.space?.x6,
+          }}
+        >
           Sempre no 220v, atrás de um filme/rolê e codando desafios em JS. Adoro trabalhar com a web e compartilhar sobre isso na Alura, Nubank e no meu canal do YouTube DevSoutinho
         </Text>
 
-        {data.posts.map(({ title, url }) => (
-          <Box
-            key={url}
-            styleSheet={{
-              marginTop: theme.space?.x4,
-              marginBottom: theme.space?.x4,
-            }}
-          >
-            <TouchableArea
-              href={url}
-              styleSheet={{
-                borderStyle: 'solid',
-                borderWidth: '10px',
-                disabled: {
-                  borderColor: theme.colors?.neutral.x500,
-                  backgroundColor: theme.colors?.neutral?.x500,
-                },
-                hover: {
-                  borderColor: theme.colors?.positive.x500,
-                  backgroundColor: {
-                    xs: theme.colors?.negative.x500,
-                  },
-                },
-                focus: {
-                  borderColor: theme.colors?.positive.x500,
-                  backgroundColor: {
-                    xs: theme.colors?.positive.x500,
-                  },
-                },
-              }}
-            >
-              <Text
-                key={title}
-                variant='body3'
-                styleSheet={{
-                  color: theme.colors?.neutral.x000,
-                }}
-              >
-                {title}
-              </Text>
-            </TouchableArea>
+        <Text
+          tag='h2'
+          variant='heading2'
+          styleSheet={{
+            width: theme.space?.['x1/1'],
+            color: theme.colors?.neutral.x100,
+            paddingBottom: theme.space?.x6,
+          }}
+        >
+          🎥 Videos Publicados
+        </Text>
+
+        {data.posts.map(({ title, url }) => {
+          const imageUrl = `https://i.ytimg.com/vi/${getYoutubeId(url)}/maxresdefault.jpg`;
+
+
+          return (
             <Box
+              key={url}
               styleSheet={{
-                backgroundColor: '#FFF',
-                padding: '50px',
+                maxWidth: '400px',
+                marginTop: theme.space?.x4,
+                marginBottom: theme.space?.x4,
               }}
             >
-              <Button
+              <TouchableArea
                 href={url}
-                label="Ver vídeo"
-              />
-              <Button
-                href={url}
-                label="Ver vídeo"
-                disabled
-              />
+              >
+                <Image
+                  src={imageUrl}
+                  styleSheet={{
+                    width: '100%',
+                    aspectRatio: {
+                      xs: 1.5,
+                      sm: 0,
+                    },
+                    borderRadius: theme.borderRadius?.md,
+                    marginBottom: theme.space?.x4,
+                  }}
+                />
+                <Text
+                  key={title}
+                  variant='body2'
+                  styleSheet={{
+                    color: theme.colors?.neutral.x200,
+                    hover: {
+                      color: theme.colors?.primary.x500,
+                    },
+                    focus: {
+                      color: theme.colors?.primary.x500,
+                    }
+                  }}
+                >
+                  {title}
+                </Text>
+              </TouchableArea>
             </Box>
-          </Box>
-        ))}
+          );
+        })}
       </Box>
     </Scaffold>
   );
