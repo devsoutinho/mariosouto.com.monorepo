f4f8eaab122a7b4034ca520c61f15e22c56c3f31
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-10 20:25:55 -0300

    update

--- packages/skynexui/core/theme/shadow/shadow.ts
+++ packages/skynexui/core/theme/shadow/shadow.ts
@@ -1,11 +1,11 @@
 export const defaultShadow = {
-  'default': '0 1px 3px rgba(0,0,0,0.5)',
-  'sm': '0 1px 2px rgba(0,0,0,0.5)',
-  'md': '0 4px 6px -1px rgba(0,0,0,0.4)',
-  'lg': '0 10px 15px rgba(0,0,0,0.4)',
-  'xl': '0 20px 25px rgba(0,0,0,0.4)',
-  'xl2': '0 25px 50px rgba(0,0,0,0.25)',
+  'default': '0 1px 3px rgba(0,0,0,0.2)',
+  'sm': '0 1px 2px rgba(0,0,0,0.2)',
+  'md': '0 4px 6px rgba(0,0,0,0.1)',
+  'lg': '0 10px 15px rgba(0,0,0,0.1)',
+  'xl': '0 20px 25px rgba(0,0,0,0.1)',
+  'xl2': '0 25px 50px rgba(0,0,0,0.05)',
   // TODO: Enable this one day
-  // 'inner': 'inset 0 2px 4px rgba(0,0,0,0.5)',
+  // 'inner': 'inset 0 2px 4px rgba(0,0,0,0.2)',
   'none': '0 0 0 #0000',
 };
