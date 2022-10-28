1f9f6e90561aa762a01eb1a51dba5d1f49503388
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-28 17:03:08 -0300

    update

--- libs/armor/lib/armor.dart
+++ libs/armor/lib/armor.dart
@@ -1,3 +1,5 @@
 library armor;
 
 export "./router/router.dart";
+export "package:url_launcher/url_launcher.dart";
+export 'package:responsive_styles/responsive_styles.dart';
--- shells/app.mariosouto.com/lib/modules/login/screens/home_screen.dart
+++ shells/app.mariosouto.com/lib/modules/login/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:armor/armor.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 // https://tailwindui.com/components/application-ui/forms/sign-in-forms#component-bc08eb211afa45fad7c9f89c1891f284
@@ -17,13 +18,80 @@ class LoginHomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You are inside the Login!',
+            Image.network(
+              "https://via.placeholder.com/56x48.jpg",
+              width: 56,
+              height: 48,
             ),
+            const Text('Sign in to your account'),
+            RichText(
+              text: TextSpan(
+                children: [
+                  const TextSpan(
+                    style: TextStyle(color: Colors.black, fontSize: 14),
+                    text: "Or ",
+                  ),
+                  TextSpan(
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    text: "start your 14-day free trial",
+                    recognizer: TapGestureRecognizer()
+                      ..onTap =
+                          () => launchUrl(Uri.parse("https://mariosouto.com/")),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: const [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text('Remember me [CHECKBOX]'),
+            const Text('Forgot your password?'),
             ElevatedButton(
               onPressed: () => go("/dashboard", context),
-              child: const Text('Go to Dashboard'),
+              child: const Text('Sign in'),
             ),
+            const Text('--------------- Or continue with ---------------'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => go("/dashboard", context),
+                  child: const Text('Twitter'),
+                ),
+                ElevatedButton(
+                  onPressed: () => go("/dashboard", context),
+                  child: const Text('Facebook'),
+                ),
+                ElevatedButton(
+                  onPressed: () => go("/dashboard", context),
+                  child: const Text('GitHub'),
+                ),
+              ],
+            ),
+            // ElevatedButton(
+            //   onPressed: () => go("/dashboard", context),
+            //   child: const Text('Go to Dashboard'),
+            // ),
           ],
         ),
       ),
