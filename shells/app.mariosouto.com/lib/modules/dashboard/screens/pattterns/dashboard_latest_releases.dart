45d320e648ab86d59dbd29bbd3555cdf6736ee39
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-11-20 23:06:10 -0300

    update

--- shells/app.mariosouto.com/lib/modules/dashboard/screens/pattterns/dashboard_latest_releases.dart
+++ shells/app.mariosouto.com/lib/modules/dashboard/screens/pattterns/dashboard_latest_releases.dart
@@ -12,6 +12,8 @@ class DashboardLatestReleases extends StatelessWidget {
           Text("Latest releases"),
           Text(
               "Carregar o último video lançado com a opção de marcar como visto e adicionar notas de estudo"),
+          Text(
+              "Importante carregar também o 'miniquiz' do vídeo, criar as perguntas de forma solta e atrelar elas no vídeo de algum jeito"),
         ],
       ),
     );
