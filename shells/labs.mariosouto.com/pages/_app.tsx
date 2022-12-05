318e540d414599fd731f443f67bbc2e61ddd9058
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-12-05 07:28:00 -0300

    update

--- shells/labs.mariosouto.com/pages/_app.tsx
+++ shells/labs.mariosouto.com/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../src/theme/tailwind-setup.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- shells/labs.mariosouto.com/pages/index.tsx
+++ shells/labs.mariosouto.com/pages/index.tsx
@@ -1,9 +1,3 @@
-export default function HomeScreen() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">
-        Welcome to DevSoutinho Labs 🧪
-      </h1>
-    </div>
-  )
-}
+import { LoginScreen } from "../src/modules/login/screen";
+
+export default LoginScreen;
